7a318513102a3521b7b3fa390c270a52d3865225
Lee Thomas <lee@ahluka.co.uk>
2019-10-23 18:49:20 +0100

    Further dicking about.

--- main.cpp
+++ main.cpp
@@ -1,20 +1,36 @@
 #include <iostream>
-#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/OpenGL.hpp>
 
 int main(int argc, char **argv) {
-	sf::RenderWindow win (sf::VideoMode(800, 600), "sfml-sandbox");
-	sf::CircleShape shape(100.0f);
+	sf::Window win(sf::VideoMode(800, 600), "sfml-sandbox");
 
-	while (win.isOpen()) {
+	win.setActive(true);
+
+	sf::Clock clock;
+	bool running = true;
+	while (running) {
 		sf::Event event;
 
 		while (win.pollEvent(event)) {
 			if (event.type == sf::Event::Closed)
-				win.close();
+				running = false;
+
+			if (event.type == sf::Event::KeyPressed) {
+				if (event.key.code == sf::Keyboard::Escape)
+					running = false;
+			}
+
+			if (event.type == sf::Event::Resized)
+				glViewport(0, 0, event.size.width, event.size.height);
 		}
 
-		win.clear();
-		win.draw(shape);
+		sf::Time elapsed = clock.restart();
+		//update(elapsed);
+		glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+
 		win.display();
 	}
 
